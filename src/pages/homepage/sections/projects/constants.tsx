b34b8eef5177cc131676ff9cf00f4f11a79a1e97
Gio <giothais.silva@gmail.com>
2023-11-25 22:15:28 -0300

    feat: update projects

--- src/pages/homepage/sections/projects/constants.tsx
+++ src/pages/homepage/sections/projects/constants.tsx
@@ -1,8 +1,38 @@
 export const cards = [
   {
-    id: "Teste",
+    id: "MoodHound",
     avatar: "https://avatars.githubusercontent.com/u/102299883?s=200&v=4",
-    text: "Sistema para gerenciamento de parcerias, onde administradores cadastram parcerias em potencial e atualizam informações acerca das reuniões ocorridas, assim monitorando o status em que a instituição se encontra no processo de fechamento de parceria",
-    link: "",
+    text: "Sistema de classificador de temas referente a atributos de produtos, serviços e processos operacionais a partir de comentários de clientes",
+    link: "https://github.com/The-Bugger-Ducks/mood-hound-documentation",
+  },
+  {
+    id: "OwlPartners",
+    avatar: "https://avatars.githubusercontent.com/u/102299883?s=200&v=4",
+    text: "Site para gerenciamento de parcerias, em parceria com a Oracle Academy",
+    link: "https://github.com/The-Bugger-Ducks/owl-partners-documentation",
+  },
+  {
+    id: "CloudFox",
+    avatar: "https://avatars.githubusercontent.com/u/102299883?s=200&v=4",
+    text: "Site para visualização de dados meteorológicos, com a montagem de uma estação com diversos tipos de sensores e um sistema capaz de ler os dados enviados por eles",
+    link: "https://github.com/The-Bugger-Ducks/cloud-fox-documentation",
+  },
+  {
+    id: "HelpDuck",
+    avatar: "https://avatars.githubusercontent.com/u/102299883?s=200&v=4",
+    text: "	Sistema Help Desk com 3 usuários: comum (abre chamados e consulta o centro de soluções), suporte (atende chamados e consulta o centro de soluções) e administrador (cadastra usuários e equipamentos e vê relatórios)",
+    link: "https://github.com/The-Bugger-Ducks/help-duck-documentation",
+  },
+  {
+    id: "Polaris",
+    avatar: "https://avatars.githubusercontent.com/u/102299883?s=200&v=4",
+    text: "Site para consumo de imagens de satélites em repositórios em nuvem, permitindo sua manipulação, visualização e download",
+    link: "https://github.com/Equipe-Polaris-DSM-2021/docs",
+  },
+  {
+    id: "ProjetoIntegrador",
+    avatar: "https://avatars.githubusercontent.com/u/79456796?s=200&v=4",
+    text: "Análise de dados com a exibição de estatísticas do eleitorado localizado no Estado de São Paulo, em parceria com a TV Vanguarda",
+    link: "https://github.com/Equipe-01-DSM-2021/projeto-integrador-2021-1",
   },
 ];
