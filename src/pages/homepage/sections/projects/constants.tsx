867efabead132c7051d5e92f0d092c28de626101
Gio <giothais.silva@gmail.com>
2023-12-04 19:28:49 -0300

    feat: update projects logos url

--- src/pages/homepage/sections/projects/constants.tsx
+++ src/pages/homepage/sections/projects/constants.tsx
@@ -1,33 +1,36 @@
 export const cards = [
   {
     id: "MoodHound",
-    avatar: "https://avatars.githubusercontent.com/u/102299883?s=200&v=4",
+    avatar:
+      "https://github.com/gioliveirass/fatec-portfolio/blob/main/src/assets/images/logo-mood-hound.png?raw=true",
     text: "Mood Hound, um sistema de classificador de temas referente a atributos de produtos, serviços e processos operacionais a partir de comentários de clientes",
     link: "https://github.com/The-Bugger-Ducks/mood-hound-documentation",
   },
   {
     id: "OwlPartners",
     avatar:
-      "https://github.com/The-Bugger-Ducks/owl-partners-web/blob/253a14d4477e14a68f662a40c5c97905131dc6bc/src/assets/logo.png?raw=true",
+      "https://github.com/gioliveirass/fatec-portfolio/blob/main/src/assets/images/logo-owl-partners.png?raw=true",
     text: "Owl Partners, um site para gerenciamento de parcerias, em parceria com a Oracle Academy",
     link: "https://github.com/The-Bugger-Ducks/owl-partners-documentation",
   },
   {
     id: "CloudFox",
     avatar:
-      "https://github.com/The-Bugger-Ducks/cloud-fox-web/blob/a66136d9a32654523ec7af60e56e70bc95f7f4d4/src/assets/logo.png?raw=true",
+      "https://github.com/gioliveirass/fatec-portfolio/blob/main/src/assets/images/logo-cloud-fox.png?raw=true",
     text: "Cloud Fox, um site para visualização de dados meteorológicos, com a montagem de uma estação com diversos tipos de sensores e um sistema capaz de ler os dados enviados por eles",
     link: "https://github.com/The-Bugger-Ducks/cloud-fox-documentation",
   },
   {
     id: "HelpDuck",
-    avatar: "https://avatars.githubusercontent.com/u/102299883?s=200&v=4",
+    avatar:
+      "https://github.com/gioliveirass/fatec-portfolio/blob/main/src/assets/images/logo-help-duck.png?raw=true",
     text: "Help Duck, um sistema Help Desk com 3 usuários: comum (abre chamados e consulta o centro de soluções), suporte (atende chamados e consulta o centro de soluções) e administrador (cadastra usuários e equipamentos e vê relatórios)",
     link: "https://github.com/The-Bugger-Ducks/help-duck-documentation",
   },
   {
     id: "Polaris",
-    avatar: "https://avatars.githubusercontent.com/u/88553952?s=200&v=4",
+    avatar:
+      "https://github.com/gioliveirass/fatec-portfolio/blob/main/src/assets/images/logo-polaris.png?raw=true",
     text: "Polaris, um site para consumo de imagens de satélites em repositórios em nuvem, permitindo sua manipulação, visualização e download",
     link: "https://github.com/Equipe-Polaris-DSM-2021/docs",
   },
