09d7774527b44fb3f9e21035dd848912872d2728
Gio <giothais.silva@gmail.com>
2023-12-04 19:02:06 -0300

    fix: responsive style of carousel

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -37,12 +37,7 @@ export default function Carousel({ cards, width }: CarouselProps) {
   const side = useBreakpointValue({ base: "30%", md: "40px" });
 
   return (
-    <Box
-      position={"relative"}
-      height={"600px"}
-      width={width}
-      overflow={"hidden"}
-    >
+    <Box position={"relative"} maxH={"450px"} width={width} overflow={"hidden"}>
       <link
         rel="stylesheet"
         type="text/css"
@@ -90,7 +85,7 @@ export default function Carousel({ cards, width }: CarouselProps) {
           >
             <Container
               size="container.lg"
-              height="600px"
+              height="24rem"
               position="relative"
               zIndex={"100"}
             >
